025b13c560b35dead10c39209d315ad0a2269e4e
Darren <darrenoc3+github@gmail.com>
2019-03-12 19:54:35 +0000

    Repository testing

--- src/main/java/com/form3/payments/config/DynamoDBConfig.java
+++ src/main/java/com/form3/payments/config/DynamoDBConfig.java
@@ -1,51 +1,55 @@
 package com.form3.payments.config;
 
 import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Regions;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
-import com.form3.payments.repository.PaymentRepository;
-import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * Beans for AWS DynamoDB Configuration
- * Copied from https://github.com/derjust/spring-data-dynamodb-examples
+ * Created with the help of https://github.com/derjust/spring-data-dynamodb-examples
  */
 @Configuration
-@EnableDynamoDBRepositories(basePackages = "com.form3.payments.repository")
 public class DynamoDBConfig {
 
+  private final Logger log = LoggerFactory.getLogger(DynamoDBConfig.class);
+
   @Value("${amazon.dynamodb.endpoint}")
-  private String dBEndpoint;
+  private String amazonDynamoDBEndpoint;
 
   @Value("${amazon.aws.accesskey}")
-  private String accessKey;
+  private String amazonAWSAccessKey;
 
   @Value("${amazon.aws.secretkey}")
-  private String secretKey;
-
+  private String amazonAWSSecretKey;
 
   @Bean
   public AmazonDynamoDB amazonDynamoDB() {
-    return AmazonDynamoDBClientBuilder.standard().withCredentials(amazonAWSCredentialsProvider())
-        .withRegion(Regions.US_EAST_1).build();
+    log.trace("Entering amazonDynamoDb()");
+    log.trace("Using Dynamo endpoint {}", amazonDynamoDBEndpoint);
+    return AmazonDynamoDBClientBuilder
+        .standard()
+        .withEndpointConfiguration(new EndpointConfiguration(amazonDynamoDBEndpoint, ""))
+        .withCredentials(new AWSStaticCredentialsProvider(amazonAWSCredentials()))
+        .build();
   }
 
-  public AWSCredentialsProvider amazonAWSCredentialsProvider() {
-    return new AWSStaticCredentialsProvider(amazonAWSCredentials());
+  @Bean
+  public DynamoDBMapper dynamoDbMapper(AmazonDynamoDB amazonDynamoDB) {
+    log.trace("Entering dynamoDbMapper()");
+    return new DynamoDBMapper(amazonDynamoDB);
   }
 
   @Bean
   public AWSCredentials amazonAWSCredentials() {
-    return new BasicAWSCredentials(accessKey, secretKey);
+    return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
   }
 }
--- src/main/java/com/form3/payments/repository/PaymentRepository.java
+++ src/main/java/com/form3/payments/repository/PaymentRepository.java
@@ -1,13 +1,46 @@
 package com.form3.payments.repository;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedList;
 import com.form3.payments.model.Payment;
 import java.util.List;
-import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
-import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 /**
  * Spring Data repository for storing Payment JSON objects keyed by Payment ID
+ * Created with the help of https://github.com/zion3mx/spring-boot-dynamodb-example
  */
-@EnableScan
-public interface PaymentRepository extends CrudRepository<Payment, String> {
+@Repository
+public class PaymentRepository {
+
+  private final Logger log = LoggerFactory.getLogger(getClass());
+
+  @Autowired
+  private DynamoDBMapper dbMapper;
+
+  public List<Payment> readAll() {
+    log.trace("Entering readAll()");
+    PaginatedList<Payment> results = dbMapper.scan(Payment.class, new DynamoDBScanExpression());
+    results.loadAllResults();
+    return results;
+  }
+
+  public Optional<Payment> read(String id) {
+    log.trace("Entering read() with {}", id);
+    return Optional.ofNullable(dbMapper.load(Payment.class, id));
+  }
+
+  public void save(Payment Payment) {
+    log.trace("Entering save() with {}", Payment);
+    dbMapper.save(Payment);
+  }
+
+  public void delete(String id) {
+    dbMapper.delete(new Payment(id));
+  }
 }
